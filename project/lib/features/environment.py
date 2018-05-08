284629533e7ad0a5c83cc88c827803fda1fbcc4e
Franco Leonardo Martínez <martinez.franco.leonardo@gmail.com>
2018-05-08 02:11:32 -0300

    Updated logging messages

--- project/lib/features/environment.py
+++ project/lib/features/environment.py
@@ -5,7 +5,8 @@ from slayer.run_framework import configure_logging
 from behave.log_capture import capture
 
 LINE_LENGTH = 42
-
+PASSED = "passed"
+FAILED = "failed"
 
 def before_all(context):
     # Setup logging for SLAYER, according to behave API reference:
@@ -14,12 +15,13 @@ def before_all(context):
 
 
 def after_all(context):
+    logging.info("%s" % ("-" * LINE_LENGTH))
     for feature in context._runner.features:
         scenarios = feature.scenarios
         logging.info("PASSED Scenarios:")
-        [logging.info(scenario.name) for scenario in scenarios if scenario.status.name == "passed"]
+        [logging.info("\t" + scenario.name) for scenario in scenarios if scenario.status.name == PASSED]
         logging.info("FAILED Scenarios:")
-        [logging.info(scenario.name) for scenario in scenarios if scenario.status.name == "failed"]
+        [logging.info("\t" + scenario.name) for scenario in scenarios if scenario.status.name == FAILED]
     logging.info("SLAYER EXECUTION FINISHED")
 
 
@@ -33,25 +35,28 @@ def after_tag(context, tag):
 
 def before_feature(context, feature):
     logging.info("%s" % ("-" * LINE_LENGTH))
-    logging.info("Feature: '{}'".format(feature.name).center(LINE_LENGTH, " "))
+    logging.info("Feature '{}' Start".format(feature.name.upper()))
+    logging.info("%s" % ("-" * LINE_LENGTH))
 
 
 def after_feature(context, feature):
-    pass
+    logging.info("Feature '{}' End".format(feature.name.upper()))
+    logging.info("%s" % ("-" * LINE_LENGTH))
 
 
 def before_scenario(context, scenario):
-    logging.info("SCENARIO: '{}'".format(scenario.name.upper()))
+    logging.info("Scenario '{}' Start".format(scenario.name.upper()))
 
 
 def after_scenario(context, scenario):
-    logging.info("'{0}' RESULT: '{1}'".format(scenario.name.upper(), scenario.status.name.upper()))
+    logging.info("Scenario '{0}' End. Result: '{1}'".format(scenario.name.upper(), scenario.status.name.upper()))
     logging.info("%s" % ("-" * LINE_LENGTH))
 
 
 def before_step(context, step):
-    logging.info("STEP: '{}'".format(step.name))
+    logging.info("Step: '{}' {:>50}".format(step.name, "# {}:{}".format(step.location.filename, step.location.line)))
 
 
 def after_step(context, step):
-    pass
+    if step.status.name == FAILED:
+        logging.info("Step: '{}': {}".format(step.name, step.status.name.upper()))
