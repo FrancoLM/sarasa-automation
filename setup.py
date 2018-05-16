40a710b34b3b1fb20d1da0e73da2897acdb41b1d
Franco Leonardo Martínez <martinez.franco.leonardo@gmail.com>
2018-05-15 23:56:24 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='slayer',
-    version='0.1.0',
+    version='0.1.0a1',
 
     description='Slayer Framework... it came to SLAY!',
 
